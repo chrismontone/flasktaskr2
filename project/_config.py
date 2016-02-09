35cc257241f4571fdc3e918eecfd52525e4170a4
chris_montone <chris@clearscienceinc.com>
2016-02-09 12:11:05 -0500

    Changed from sqlite3 to SQLAlchemy

--- project/_config.py
+++ project/_config.py
@@ -1,6 +1,5 @@
 # project/_config.py
 
-
 import os
 
 # grab the folder where this script lives
@@ -14,3 +13,6 @@ SECRET_KEY = 'my_precious'
 
 # define the full path for the database
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# the database uri
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
