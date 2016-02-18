7eeb03c20449920cb63b3c7f84c47a4c6bf4556d
Chris Montone <chris.montone@gmail.com>
2016-02-18 09:26:31 -0500

    Added custom error page handling

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,6 +16,7 @@ class MainTests(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
+        app.config['DEBUG'] = False
         app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
             os.path.join(basedir, TEST_DB)
         self.app = app.test_client()
@@ -42,6 +43,7 @@ class MainTests(unittest.TestCase):
     def test_404_error(self):
     	response = self.app.get('/this-route-does-not-exist/')
     	self.assertEquals(response.status_code, 404)
+        self.assertIn(b'Sorry. There\'s nothing here.', response.data)
 
     def test_500_error(self):
     	bad_user = User(
@@ -53,6 +55,8 @@ class MainTests(unittest.TestCase):
     	db.session.commit()
     	response = self.login('Jeremy', 'django')
     	self.assertEquals(response.status_code, 500)
+        self.assertNotIn(b'ValueError: Invalid salt', response.data)
+        self.assertIn(b'Something went terribly wrong.', response.data)
 
     if __name__ == "__main__":
     	unitest.main()
